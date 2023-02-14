9f18bb79467db314484e6119507a6f9c0da50a44
gskrentny <skrentnyfam@gmail.com>
2023-02-13 23:04:28 -0800

    add main.cpp

---
+++ main.cpp
@@ -0,0 +1,14 @@
+// Grace Skrentny
+// Software Engineering
+// 02.13.2023
+// main.cpp 
+// SE05ContInt
+// a simple C++ file for github practices
+
+#include<iostream?
+using namespace std;
+
+int main(){
+	cout<<"blessings!"<<endl;
+return 0;
+}
